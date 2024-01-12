852b9130113a8d98f052ecb7d086f924bb9493a6
safnaahamed <safna_ahamed@yahoo.co.in>
2024-01-12 21:43:14 +0530

    Constructor with super updated

--- Polymorphism_sample/src/com/polymorphism/PClass1.java
+++ Polymorphism_sample/src/com/polymorphism/PClass1.java
@@ -1,10 +1,24 @@
 package com.polymorphism;
 
 public class PClass1 {
-	
+	int a, b, c;
+
+	public PClass1() {
+
+		System.out.println("Constructor in parent class");
+	}
+
 	public void firstMethod() {
 		System.out.println("I am a mother");
-		
+
+	}
+
+	public void add() {
+
+		a = 10;
+		b = 20;
+		c = a + b;
+		System.out.println("The sum is = " + c);
 	}
 
 }
--- Polymorphism_sample/src/com/polymorphism/PClass2.java
+++ Polymorphism_sample/src/com/polymorphism/PClass2.java
@@ -1,16 +1,22 @@
 package com.polymorphism;
 
-public class PClass2 extends PClass1{
-	
+public class PClass2 extends PClass1 {
+
+	public PClass2() {
+		super();
+		System.out.println("Constructor in child class");
+	}
+
 	public void firstMethod() {
 		System.out.println("I am a full time employee");
 		super.firstMethod();
+		super.add();
 	}
 
 	public static void main(String[] args) {
 		PClass2 p2 = new PClass2();
 		p2.firstMethod();
-		
+		//p2.add();
 
 	}
 
